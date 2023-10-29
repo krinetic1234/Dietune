6453f2ca745341aa0fd8f328cfa4e4b8b2a4d93f
jaixbhatia <jaixbhatia@gmail.com>
2023-10-28 18:07:22 -0700

    updated repo

--- Dietune/Dietune.py
+++ Dietune/Dietune.py
@@ -1,12 +1,60 @@
-"""Welcome to Reflex!."""
-
-from Dietune import styles
-
-# Import all the pages.
-from Dietune.pages import *
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+from rxconfig import config
 
 import reflex as rx
 
-# Create the app and compile it.
-app = rx.App(style=styles.base_style)
+docs_url = "https://reflex.dev/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+rx.toggle_color_mode
+
+
+class FormState(rx.State):
+
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        self.form_data = form_data
+
+
+def index():
+    return rx.vstack(
+        rx.toggle_color_mode,
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="Weight",
+                    id="weight",
+                ),
+                rx.input(
+                    placeholder="Height",
+                    id="height"
+                ),
+                rx.input(
+                    placeholder="Age",
+                    id="age"
+                ),
+                rx.input(
+                    placeholder="Sex",
+                    id="sex"
+                ),
+                rx.input(
+                    placeholder="Fitness Goal",
+                    id="fitness_goal"
+                ),
+                rx.input(
+                    placeholder="Activity",
+                    id="activity"
+                ),
+                rx.button("Submit", type_="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+        ),
+        rx.divider(),
+        rx.heading("Results"),
+        rx.text(FormState.form_data.to_string()),
+        rx.text(FormState.form_data["weight"])
+    )
+
+app = rx.App()
+app.add_page(index)
 app.compile()
