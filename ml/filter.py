82d3e52a2e0eae79f0a538d5d1c6c98a94bcc933
jaixbhatia <66648216+jaixbhatia@users.noreply.github.com>
2023-10-29 00:12:44 -0700

    Update filter.py

--- ml/filter.py
+++ ml/filter.py
@@ -1,48 +1,61 @@
-#===========STEP 3: FOOD RECS (FILTER) ===========
+import reflex as rx
 import pandas as pd
 
-file_path = "data/foods.csv"
-df = pd.read_csv(file_path)
+file_path = "/Users/jaibhatia/Desktop/converted_word_lists.csv"
 
-default_values = {
-    "quantity": 0,  # Default value for numeric columns
-    "serving_size": 0,  # Default value for numeric columns
-    "ingredients_text": "N/A",  # Default value for string columns
-    "allergens": "N/A",  # Default value for string columns
-    "traces_en": "N/A",  # Default value for string columns
-    "additives_en": "N/A",  # Default value for string columns
-    "main_category_en": "N/A",  # Default value for string columns
-    "energy_100g": 0,  # Default value for numeric columns
-    "fat_100g": 0,  # Default value for numeric columns
-    "cholesterol_100g": 0,  # Default value for numeric columns
-    "carbohydrates_100g": 0,  # Default value for numeric columns
-    "sugars_100g": 0,  # Default value for numeric columns
-    "fiber_100g": 0,  # Default value for numeric columns
-    "proteins_100g": 0,  # Default value for numeric columns
-    "sodium_100g": 0,  # Default value for numeric columns
-    "calcium_100g": 0,  # Default value for numeric columns
-    "iron_100g": 0,  # Default value for numeric columns
-}
+#  load database + filter
+df = pd.read_csv(file_path)[['name', 'nutrition']]
+# name                                     nutrition
+# crab filled crescent snacks          [69.2, 3.0, 9.0, 6.0, 5.0, 4.0, 3.0]
+# Nutrition information (calories (#), total fat (PDV), sugar (PDV) , sodium (PDV) , protein (PDV) , saturated fat (PDV) , and carbohydrate (PDV)
 
-# using provided info about macros
 
-# based on user input
-user_input = {
-    "proteins_100g": 50,
-    "sugars_100g": 5,
-}
+# convert PDV -> grams (source: https://www.fda.gov/food/nutrition-facts-label/daily-value-nutrition-and-supplement-facts-labels)
+PDV_protein_g = 50
+PDV_fat_g = 78
+PDV_carbs_g = 275
 
-mask = pd.Series(True, index=df.index)
-for column, value in user_input.items():
-    if column in default_values:
-        threshold = value if value is not None else default_values[column]
-        if column in df:
-            column_values = df[column].apply(lambda x: float(x) if x != "N/A" else default_values[column])
-            mask &= (column_values >= threshold)
-filtered_df = df[mask]
+for _, row in df.iterrows():
+    eval(row['nutrition'])[1] *= PDV_fat_g
+    eval(row['nutrition'])[4] *= PDV_protein_g
+    eval(row['nutrition'])[6] *= PDV_carbs_g
 
-items = {}
-limit = 4
+# give recommendation foods (target protein, fat, carbs --> list of Recommendation)
+class Recommendation(rx.Base):
+    name: str
+    protein: float
+    fat: float
+    carbs: float
 
-for i, row in filtered_df.iterrows():
-    items[row['product_name']] = Item(**row.to_dict())
+def give_recommendations(df, protein, fat, carbs, error_percentage):
+    # Create an empty list to store matching Recommendation objects
+    recommendations = []
+    
+    # Iterate through each row in the DataFrame
+    for _, row in df.iterrows():
+        # Extract the nutrition values from the row
+        row_fat = row['nutrition'][1]
+        row_protein = row['nutrition'][4]
+        row_carbs = row['nutrition'][6]
+        
+        # Calculate the allowable error based on the error percentage
+        fat_error = fat * error_percentage / 100
+        protein_error = protein * error_percentage / 100
+        carbs_error = carbs * error_percentage / 100
+        
+        # Check if the nutrition values are within the specified range
+        if (
+            (fat - fat_error <= row_fat <= fat + fat_error) and
+            (protein - protein_error <= row_protein <= protein + protein_error) and
+            (carbs - carbs_error <= row_carbs <= carbs + carbs_error)
+        ):
+            # Create a Recommendation object and append it to the list
+            recommendation = Recommendation(
+                name=row['name'],
+                protein=row_protein,
+                fat=row_fat,
+                carbs=row_carbs
+            )
+            recommendations.append(recommendation)
+    
+    return recommendations
